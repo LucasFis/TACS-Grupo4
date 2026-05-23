94a185c9e335024cdf0504161b615f8e8a44ff7d
LucasFis <lfiscella@frba.utn.edu.ar>
2026-05-23 15:08:58 -0300

    Desactivacion de transacciones en los test

--- backend/src/main/java/app/MongoConfig.java
+++ backend/src/main/java/app/MongoConfig.java
@@ -2,12 +2,14 @@ package app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 
 @Configuration
 public class MongoConfig {
   @Bean
+  @Profile("!test")
   public MongoTransactionManager transactionManager(MongoDatabaseFactory factory) {
     return new MongoTransactionManager(factory);
   }
